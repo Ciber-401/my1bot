2fe72ce2431a881abdb4639b551e555d7fdf35e2
Stalker <gangsta.okulus02@gmail.com>
2021-12-18 11:16:17 +0600

    bot

--- Telebot/sozdanie.py
+++ Telebot/sozdanie.py
@@ -3,10 +3,10 @@ from telebot import types
 
 bot = telebot.TeleBot('5049822153:AAF_vCDWlGtTsR2dfasB9-inwQ-PEjzsVCM')
 #  qwer это старт бота
-@bot.message_handler(commands=['qwer'])
+@bot.message_handler(commands=['start'])
 def welcom(message): # создаем функцию которая приветствует пользователя при нажатии старта
 
-    photo = open('app.jpeg', 'rb') # перердаем с помошью функции "open" картинку в формате "jpeg"
+    photo = open('iphone.jpeg', 'rb') # перердаем с помошью функции "open" картинку в формате "jpeg"
 
     bot.send_photo(message.chat.id, photo) # тут предаем message-в чат телеграмма photo это переменная
 
@@ -15,28 +15,30 @@ def welcom(message): # создаем функцию которая привет
                                                              # 3.ReplyKeyboardMarkup
                                                              # 4.resize_keyboard=True пускай выводит мне следуюшие кнопки
 
-    button1 = types.KeyboardButton('Идем дальше') # первая созданная кнопка которая высвечивает
-                                         # сообщение 'Идем дальше'
+    button1 = types.KeyboardButton('Могу приступить к осмотру моделей') # первая созданная кнопка которая высвечивает
+                                                   # сообщение 'Идем дальше'
     bot.send_message(message.chat.id,
-                     f'Привет, тут у тебя есть возможность найти  {message.from_user}',
+                     f' "#" {message.from_user}',
                      reply_markup=markup)
 
-    button2 = types.KeyboardButton('Купить смартфон') # тут добавляем кнопку еше одну с помошью переменной 'button2'
+    button2 = types.KeyboardButton('"Купить смартфон"') # тут добавляем кнопку еше одну с помошью переменной 'button2'
                                                 # и передаем ей тип кнопки 'types'-'KeyboardButton'
 
     markup.add(button1,button2) # в переменну 'markup' мы добавляем
                                 # эти две переменные в которых есть кнопки "button1", "button2"
-# создаем декоратор в которую заварачиваем все кнопки
-@bot.message_handler(content_types = ['text'])
-def aswer(message):
+# создаем декоратор в которую заварачиваем новые кнопки и действия к ним
+@bot.message_handler(content_types = ['text']) # Декоратор
+def aswer(message): # функция отвечать на конпки
 
-    if message.text == "GO":
-        bot.send_message(message.chat.id, 'welcom to eyes sauron')
-    elif message.text == 'BUYiphone':
+    if message.text == "#":  # если нажать на кнопку  " # "
+        bot.send_message(message.chat.id, '#') # то выведи мне следующее сообщение
+    elif message.text == '#': # а еше если нажмет на кнопку " # " то выведи следующие типы марок смартфона
 
-        markup = types.InlineKeyboardMarkup(row_width=3)
+        markup = types.InlineKeyboardMarkup(row_width=3) # в начале обозначаете тип кнопок для следующих марок смартфона
 
-        button3= types.InlineKeyboardButton("iphone",
+        button3= types.InlineKeyboardButton("iphone", # создаёте переменную
+                                                      # в которую указвается тип кнопки и "имя кнопки"
+                                                      # дальще callback_data="имя кнопки "
                                             callback_data='iphone')
 
         button4 = types.InlineKeyboardButton("iphone 2",
@@ -45,20 +47,22 @@ def aswer(message):
         button5 = types.InlineKeyboardButton("iphone 3",
                                              callback_data='iphone 3')
 
-        markup.add(button3,button4,button5)
+        markup.add(button3,button4,button5) # обьединяете все кнопки с помощью метода .add()
+                                            # добавляете в ранее созданном переменном (markup) где указан тип кнопки
 
-        bot.send_message(message.chat.id,
+        bot.send_message(message.chat.id, # вместе с кнопками высветилась сообщение
+                                          # передаёте ее в bot.send_message(message.chat.id, " # ", reply_markup=markup)
                          'Какую марку айфона вы хотите?',
                          reply_markup=markup)
     else:
-        bot.send_message(message.chat.id, 'error')
+        bot.send_message(message.chat.id, 'error') # в другом случае показывай сообщение "error"
 
-@bot.callback_query_handler(func=lambda call : True)
-def callback_inline(call):
-    try:
-        if call.message:
-            if call.data == 'iphone':
-                bot.send_message(call.message.chat.id,
+@bot.callback_query_handler(func=lambda call : True) # заварачиваем это все в декоратор в котором
+def callback_inline(call): # создаем новую функцию которая отвечает за обратный вызов то есть ответ на кнопки
+    try: # Засовываем это все в проверку
+        if call.message: # если " call " это message то
+            if call.data == 'iphone':  # и если нажали на кнопку " iphone "
+                bot.send_message(call.message.chat.id, #  то высвети следующее сообщение
                                  '500$')
 
             if call.data == 'iphone 2':
@@ -71,7 +75,5 @@ def callback_inline(call):
 
     except:
         print('error')
-
-
 
 bot.polling(none_stop=True)
